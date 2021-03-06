17d43eb24ed3be81c2fb7b55118dbb3c6c2b6540
Chanawee124 <78196908+Chanawee124@users.noreply.github.com>
2021-03-06 17:22:48 +0700

    Update main.c

--- main.c
+++ main.c
@@ -3,19 +3,32 @@
 #include<string.h>
 #include "stack.h"
 
-int main(int argc, char *argv[]){
+int main(int argc, char **argv){
   printf("Checking the parentheses in argv arguments\n");
-  int i,N,j,x;
+  int i,N,j,n;
+  n = 0;
   
-  for(i=1;i<argc;i++){
-    //for(j=0;j<strlen(argv[i]);j++){
-      if (areBracketsBalanced(argv[i]))
-        printf("The parentheses match successfully for %s \n",argv[i]);
+  Stack s;
+  s.size = 0;
+  s.top = NULL;
+  
+  for(i=1;i<argc;i++){   
+     for(j=0;j<strlen(argv[i]);j++){
+      switch(argv[i][j]){
+        case '[' :
+        case '{' :  push(&s,argv[i][j]); break;
+        case ']' :  if(pop(&s) != '[') n=1; break;
+        case '}' :  if(pop(&s) != '{') n=1; break;
+      }
+      if(n == 1) break;
+      }
+      if(n == 1 || s.size > 0)
+      printf("The parentheses do not match for %s\n",argv[i]);
       else
-        printf("The parentheses do not match for %s \n",argv[i]);
-        pop_all(ll);
-      //}
+      printf("The parentheses match successfully for %s\n",argv[i]);
+      n=0;
+      if(s.size > 0)
+        pop_all(&s);
   }
-  
-return 0;
+    return 0;
 }
